0a6b7135ef77758f65e852149bfa04833f3833ca
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-02-14 04:45:39 +0000

    Add files via upload

--- fxapp/src/main/java/somuna/fxapp/FXApp.java
+++ fxapp/src/main/java/somuna/fxapp/FXApp.java
@@ -2,7 +2,6 @@ package somuna.fxapp;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import somuna.fxapp.views.View;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
@@ -10,9 +9,8 @@ public class FXApp extends Application{
 	protected Model model;
 	protected View view;
 	protected Controller controller;
-	private Pane root = new Pane();
-	private Scene scene = new Scene(root, 800, 600);
-		
+	private final Pane root = new Pane();
+	private final Scene scene = new Scene(root, 800, 600);
 	public static void main(String[] args) {
 		launch(args);
 	}
